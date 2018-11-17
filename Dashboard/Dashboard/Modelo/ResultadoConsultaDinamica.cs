c6106477a3f7454380fb36032e92d5cdb742669d
bfkathe <bfkathe@gmail.com>
2018-11-17 15:18:34 -0600

    Archivo de proyecto

---
+++ Dashboard/Dashboard/Modelo/ResultadoConsultaDinamica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Modelo
+{
+    class ResultadoConsultaDinamica : IResultado
+    {
+        public void getResultado()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dashboard/Dashboard/Modelo/ResultadoTablas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Modelo
+{
+    class ResultadoTablas : IResultado
+    {
+        public void getResultado()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
